ca5fcde1d25d4d26666f7e0cd434b5b43a434f51
김구태 <gootae0220@naver.com>
2025-04-29 10:15:11 +0900

    feat : mqtt 클라이언트 연결 및 websocket 엔드포인트 추가

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
 # FastAPI 실행하는 파일
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import mqtt_client
+
+
+app = FastAPI()
+
+# FastAPI 애플리케이션이 시작될 때 MQTT 클라이언트 연결
+@app.on_event("startup")
+async def startup_event():
+    # mqtt 클라이언트 연결
+    mqtt_client.connect_mqtt()
+
+# 루트 경로를 정의하여 404 오류 방지
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the FastAPI MQTT WebSocket server!"}
+
+# 웹소켓 엔드포인트
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    mqtt_client.websockets.add(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        mqtt_client.websockets.remove(websocket)
